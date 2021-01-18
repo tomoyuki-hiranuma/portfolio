09e5a667d8c0bd8ab5d9e3c25ab420db297dd494
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-18 18:17:20 +0900

    create base header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MyHeader,
+  },
+};
 </script>
 
 <style>
