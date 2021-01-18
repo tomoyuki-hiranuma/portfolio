b3c0d4f9cb040e6286207d38d8b6ee248ca134ac
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-18 20:34:16 +0900

    create footer base

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>&copy;{{ thisYear }} Tomoyuki Hiranuma. All rights reserved.</p>
+    <p>Github</p>
+    <p>Twitter</p>
+    <p>Blog</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'MyFooter',
+  data() {
+    return {
+      thisYear: '2021-',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  background-color: lighten(dimgray, 30%);
+  padding-top: 20px;
+}
+</style>
