ee9989b0dfdd2481261d707497947c8bf9b5daef
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-19 01:34:20 +0900

    about me base created

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <p>About me</p>
+    <p class="text-4xl my-10">About me</p>
+    <div class="text-lg my-10">
+      <li>Master student at Tokyo Institute of Technology</li>
+      <li>Major in Conputer Science</li>
+      <li>Study about Genetic Algorithm in Combinatorial Optimization</li>
+      <li>Web developer</li>
+    </div>
   </div>
 </template>
 
