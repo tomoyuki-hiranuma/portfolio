828c0c36009bbadbfb51edd5c59cf49befdab1e2
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-19 21:44:08 +0900

    create Education

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,6 +6,8 @@
       <li>Major in Conputer Science</li>
       <li>Study about Genetic Algorithm in Combinatorial Optimization</li>
       <li>Web developer</li>
+      <p>I want to be a software engineer working around the world,</p>
+      <p>and make application give the big impact to the world.</p>
     </div>
   </div>
 </template>
---
+++ src/components/Education.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p class="text-4xl my-10">Education</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Education',
+};
+</script>
