46765875791419469a1c2642747e8937081fba84
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-28 17:54:55 +0900

    add todo message

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p class="text-4xl my-10 font-serif">Skills</p>
+    <!-- TODO: card componentにして使い回すようにする -->
     <div class="container mx-auto grid grid-cols-3 justify-center items-center">
       <div
         class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
@@ -58,6 +59,18 @@
         </div>
         <div class="flex-grow p-3">
           <div class="font-semibold text-gray-700">Python</div>
+          <div class="text-sm">★★★☆☆</div>
+          <div class="text-sm text-gray-500">コメント</div>
+        </div>
+      </div>
+      <div
+        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
+      >
+        <div class="flex items-center space-x-4">
+          <p>C++ img</p>
+        </div>
+        <div class="flex-grow p-3">
+          <div class="font-semibold text-gray-700">C++</div>
           <div class="text-sm">★★★☆☆</div>
           <div class="text-sm text-gray-500">コメント</div>
         </div>
