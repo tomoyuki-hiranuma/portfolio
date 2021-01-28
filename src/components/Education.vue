d2b4ebf06e52c990974fd762312af9718765b91d
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-28 17:18:01 +0900

    add research

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -2,12 +2,16 @@
   <div>
     <p class="text-4xl my-10 font-serif">Education</p>
     <div>
-      <li class="p-3">
-        Department of Mechanical Engineering Infomatics, Meiji Univ
-      </li>
-      <li class="p-3">
-        Computer Science, School of Computing, Tokyo Institute of Technology
-      </li>
+      <div class="p-3">
+        <li>Department of Mechanical Engineering Infomatics, Meiji Univ</li>
+        <p>Bachelar's research: control engineering</p>
+      </div>
+      <div class="p-3">
+        <li>
+          Computer Science, School of Computing, Tokyo Institute of Technology
+        </li>
+        <p>Master's research: Mathematical optimization, Genetic Algorithm</p>
+      </div>
     </div>
   </div>
 </template>
