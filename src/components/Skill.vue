e3d637d28b6f963bc2b6817febdb69fa8fdc4abd
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-28 22:56:55 +0900

    v-forで繰り返す

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <p class="text-4xl my-10 font-serif">Skills</p>
-    <!-- TODO: card componentにして使い回すようにする -->
-    <!-- TODO: props に渡す -->
     <div
       class="container mx-auto grid grid-cols-3 gap-3 justify-center items-center"
     >
-      <SkillCard />
-      <SkillCard />
-      <SkillCard />
-      <SkillCard />
-      <SkillCard />
+      <div v-for="skill in skills" v-bind:key="skill.program">
+        <SkillCard
+          :program="skill.program"
+          :level="skill.level"
+          :comment="skill.comment"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +22,54 @@ export default {
   name: 'Skill',
   components: {
     SkillCard,
+  },
+  data: function () {
+    return {
+      skills: [
+        {
+          program: 'Ruby on Rails',
+          level: '★★★★☆',
+          comment: '調べながらある程度のアプリ作れます',
+        },
+        {
+          program: 'C++',
+          level: '★★★☆☆',
+          comment: '競技プログラミングに使用しています',
+        },
+        {
+          program: 'Python',
+          level: '★★★☆☆',
+          comment: '研究で使用',
+        },
+        {
+          program: 'HTML5/CSS3',
+          level: '★★★☆☆',
+        },
+        {
+          program: 'JavaScript(ES6)',
+          level: '★★★☆☆',
+        },
+        {
+          program: 'Vue.js',
+          level: '★★☆☆☆',
+          comment: '本サイトに使用',
+        },
+        {
+          program: 'Git',
+          level: '★★★☆☆',
+        },
+        {
+          program: 'AWS',
+          level: '★★★☆☆',
+          comment: 'S3, EC2, RDS',
+        },
+        {
+          program: 'Docker',
+          level: '★★★☆☆',
+          comment: 'Docker, docker-compose',
+        },
+      ],
+    };
   },
 };
 </script>
