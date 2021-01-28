573bf2d7d44cdb36b473031183de7c3c81ae1a0f
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-28 18:00:13 +0900

    add skill-card component

---
+++ src/atoms/SkillCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white w-full flex items-center p-2 rounded-xl shadow border">
+    <div class="flex items-center space-x-4">
+      <p>C++ img</p>
+    </div>
+    <div class="flex-grow p-3">
+      <div class="font-semibold text-gray-700">C++</div>
+      <div class="text-sm">★★★☆☆</div>
+      <div class="text-sm text-gray-500">コメント</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SkillCard',
+};
+</script>
--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -3,84 +3,22 @@
     <p class="text-4xl my-10 font-serif">Skills</p>
     <!-- TODO: card componentにして使い回すようにする -->
     <div class="container mx-auto grid grid-cols-3 justify-center items-center">
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="flex items-center space-x-4">
-          <p>html img</p>
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">HTML5/CSS3</div>
-          <div class="text-sm">★★☆☆☆</div>
-          <div class="text-sm text-gray-500">コメント</div>
-        </div>
-      </div>
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="flex items-center space-x-4">
-          <p>jQuery img</p>
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">jQuery</div>
-          <div class="text-sm">★★☆☆☆</div>
-          <div class="text-sm text-gray-500">コメント</div>
-        </div>
-      </div>
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="flex items-center space-x-4">
-          <p>Vue img</p>
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">Vue.js</div>
-          <div class="text-sm">★★☆☆☆</div>
-          <div class="text-sm text-gray-500">コメント</div>
-        </div>
-      </div>
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="flex items-center space-x-4">
-          <p>Ruby img</p>
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">Ruby</div>
-          <div class="text-sm">★★★☆☆</div>
-          <div class="text-sm text-gray-500">コメント</div>
-        </div>
-      </div>
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="flex items-center space-x-4">
-          <p>Python img</p>
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">Python</div>
-          <div class="text-sm">★★★☆☆</div>
-          <div class="text-sm text-gray-500">コメント</div>
-        </div>
-      </div>
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="flex items-center space-x-4">
-          <p>C++ img</p>
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">C++</div>
-          <div class="text-sm">★★★☆☆</div>
-          <div class="text-sm text-gray-500">コメント</div>
-        </div>
-      </div>
+      <SkillCard />
+      <SkillCard />
+      <SkillCard />
+      <SkillCard />
+      <SkillCard />
     </div>
   </div>
 </template>
 
 <script>
+import SkillCard from '../atoms/SkillCard';
+
 export default {
   name: 'Skill',
+  components: {
+    SkillCard,
+  },
 };
 </script>
