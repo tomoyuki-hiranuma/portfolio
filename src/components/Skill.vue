a8f7966fa8ad423b46e49a20b236d022144a1456
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-28 17:50:43 +0900

    add skills cards

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,6 +1,68 @@
 <template>
   <div>
     <p class="text-4xl my-10 font-serif">Skills</p>
+    <div class="container mx-auto grid grid-cols-3 justify-center items-center">
+      <div
+        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
+      >
+        <div class="flex items-center space-x-4">
+          <p>html img</p>
+        </div>
+        <div class="flex-grow p-3">
+          <div class="font-semibold text-gray-700">HTML5/CSS3</div>
+          <div class="text-sm">★★☆☆☆</div>
+          <div class="text-sm text-gray-500">コメント</div>
+        </div>
+      </div>
+      <div
+        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
+      >
+        <div class="flex items-center space-x-4">
+          <p>jQuery img</p>
+        </div>
+        <div class="flex-grow p-3">
+          <div class="font-semibold text-gray-700">jQuery</div>
+          <div class="text-sm">★★☆☆☆</div>
+          <div class="text-sm text-gray-500">コメント</div>
+        </div>
+      </div>
+      <div
+        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
+      >
+        <div class="flex items-center space-x-4">
+          <p>Vue img</p>
+        </div>
+        <div class="flex-grow p-3">
+          <div class="font-semibold text-gray-700">Vue.js</div>
+          <div class="text-sm">★★☆☆☆</div>
+          <div class="text-sm text-gray-500">コメント</div>
+        </div>
+      </div>
+      <div
+        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
+      >
+        <div class="flex items-center space-x-4">
+          <p>Ruby img</p>
+        </div>
+        <div class="flex-grow p-3">
+          <div class="font-semibold text-gray-700">Ruby</div>
+          <div class="text-sm">★★★☆☆</div>
+          <div class="text-sm text-gray-500">コメント</div>
+        </div>
+      </div>
+      <div
+        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
+      >
+        <div class="flex items-center space-x-4">
+          <p>Python img</p>
+        </div>
+        <div class="flex-grow p-3">
+          <div class="font-semibold text-gray-700">Python</div>
+          <div class="text-sm">★★★☆☆</div>
+          <div class="text-sm text-gray-500">コメント</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
