cd1f9555d0576ca1a442f62264db89fe38017d58
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-31 16:45:31 +0900

    fix: intern layout

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -18,19 +18,19 @@
     <!-- 変数にしてfor文で回す -->
     <div>
       <!-- コンポーネントに分ける -->
-      <div class="my-10">
-        <p class="">TeamLab Inc.</p>
-        <div>
-          <p>2020-11-26 ~ 2020-12-23</p>
+      <div class="py-5 flex container mx-auto">
+        <div class="flex-1 my-auto text-xl font-medium">TeamLab Inc.</div>
+        <div class="flex-1">
+          <p>2020/11/26 ~ 2020/12/23</p>
           <p>
             Work as a backend engineer in the e-commerce technology actually
           </p>
         </div>
       </div>
-      <div class="my-10">
+      <div class="py-5">
         <p class="">Rakuten Inc.</p>
         <div>
-          <p>2020-9-1 ~ 2020-9-11</p>
+          <p>2020/9/1 ~ 2020/9/11</p>
           <p>
             Develop a new service as a prototype in the theme "Connect people
             online"<br />
@@ -39,17 +39,17 @@
           </p>
         </div>
       </div>
-      <div class="my-10">
+      <div class="py-5">
         <p>e-Seikatsu Inc.</p>
         <div>
-          <p>2020-8-24 ~ 2020-8-28</p>
+          <p>2020/8/24 ~ 2020/8/28</p>
           <p>Develop a Property search service using in-house API</p>
         </div>
       </div>
-      <div class="my-10">
+      <div class="py-5">
         <p>GA Technologies Inc.</p>
         <div>
-          <p>2020-6-27 ~ 2020-6-28</p>
+          <p>2020/6/27 ~ 2020/6/28</p>
           <p>
             Develop a prototype service that overcomes to replace with furniture
           </p>
