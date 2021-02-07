19cf34f8e3b2f66a5ba66e64b90eb2c6348589b8
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-02-07 18:30:34 +0900

    add: basically create works

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -2,7 +2,46 @@
   <div id="works">
     <p class="text-4xl my-10 font-serif">Works</p>
     <div>
-      <p>工事中...成果物まとめる</p>
+      <div class="my-5">
+        <p>オンライン衣類計測サービス</p>
+        <div>Vignette&Clarity社のサービス</div>
+        <div>
+          <div>使用技術</div>
+          <li>Ruby on Rails</li>
+          <li>BootStrap</li>
+          <li>AWS(S3, RDB, EC2)</li>
+          <li>Flask</li>
+        </div>
+      </div>
+      <div class="my-5">
+        <p>TheaTalk</p>
+        <div>楽天株式会社サマーインターンで開発</div>
+        <div>
+          <div>使用技術</div>
+          <li>Ruby on Rails</li>
+          <li>MaterialUI</li>
+          <li>React.js</li>
+          <li>Redux</li>
+        </div>
+      </div>
+      <div class="my-5">
+        <p>大学生にいい生活</p>
+        <div>いい生活株式会社サマーインターンで開発</div>
+        <div>
+          <div>使用技術</div>
+          <li>Vue.js</li>
+          <li>Firebase</li>
+        </div>
+      </div>
+      <div class="my-5">
+        <p>レイアウトレコメンドサービス</p>
+        <div>GATechnologies株式会社ハッカソンインターンで開発</div>
+        <div>
+          <div>使用技術</div>
+          <li>Ruby on Rails</li>
+          <li>BootStrap</li>
+        </div>
+      </div>
     </div>
   </div>
 </template>
