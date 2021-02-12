3c6f7b20a19d06fc798f09cf2b75a5ee8c94b97e
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-02-13 01:01:03 +0900

    add: css to works

--- src/atoms/WorksCard.vue
+++ src/atoms/WorksCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="py-10 flex justify-center">
     <div class="mx-10">
-      <p>{{ work_name }}</p>
-      <div>{{ where }}</div>
-      <div>
+      <p class="font-extrabold text-xl font-serif">{{ work_name }}</p>
+      <div class="text-sm">{{ where }}</div>
+      <div class="container border-4 my-2 py-2 border-pink-300">
         <div>使用技術</div>
         <div v-for="skill in skills" v-bind:key="skill.index">
-          <li>{{ skill }}</li>
+          <li class="text-left mx-10">{{ skill }}</li>
         </div>
       </div>
     </div>
