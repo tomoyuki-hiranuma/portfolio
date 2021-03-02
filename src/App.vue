3e7ab4d97c609ee4810fdafac1eecf0380ac1b4a
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-03 01:22:56 +0900

    responsive: top margin in top

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="mt-5 md:mt-16">
     <MyHeader />
     <Main />
     <MyFooter />
@@ -28,6 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
