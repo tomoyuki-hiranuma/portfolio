e318980d73f4ef0e994abf5a1c0d53741e17d6a9
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-13 22:32:49 +0900

    fix assign about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="about">
     <p class="text-2xl md:text-4xl my-7 md:my-10 font-serif">About me</p>
-    <div class="text-sm md:text-lg my-7 md:my-10">
-      <li>Master student at Tokyo Institute of Technology</li>
-      <li>Major in Computer Science</li>
-      <li>Study about Genetic Algorithm in Combinatorial Optimization</li>
-      <li>Web developer</li>
-      <p>I want to be a software engineer working around the world,</p>
-      <p>and make applications like making the world more convenient.</p>
+    <div class="w-1/2 mx-auto text-sm md:text-lg my-7 md:my-10">
+      <div class="">
+        <li class="text-left">
+          Master student at Tokyo Institute of Technology
+        </li>
+        <li class="text-left">Major in Computer Science</li>
+        <li class="text-left">
+          Study about Genetic Algorithm in Combinatorial Optimization
+        </li>
+        <li class="text-left">Web developer</li>
+        <p>I want to be a software engineer working around the world,</p>
+        <p>and make applications like making the world more convenient.</p>
+      </div>
     </div>
   </div>
 </template>
