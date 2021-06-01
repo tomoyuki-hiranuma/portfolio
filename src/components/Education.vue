6e6e092db11839f522b744497d16b5bca12fb928
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-06-01 17:27:25 +0900

    fix text line in educations

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="education">
     <p class="text-2xl md:text-4xl my-7 md:my-10 font-serif">Education</p>
-    <div class="text-sm md:text-lg">
+    <div class="text-sm md:text-lg w-1/2 mx-auto">
       <div class="p-1 md:p-3">
-        <li>Department of Mechanical Engineering Infomatics, Meiji Univ</li>
+        <li class="text-left">
+          Department of Mechanical Engineering Infomatics, Meiji Univ
+        </li>
         <p>Bachelor's research: control engineering</p>
       </div>
       <div class="p-1 md:p-3">
-        <li>
+        <li class="text-left">
           Computer Science, School of Computing, Tokyo Institute of Technology
         </li>
         <p>Master's research: Mathematical optimization, Genetic Algorithm</p>
