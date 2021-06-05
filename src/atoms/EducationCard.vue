b99b1b39451cc9c0208ba0ff5368dc54e8101497
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-06-05 15:05:55 +0900

    refactoring

---
+++ src/atoms/EducationCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-10 border-4 rounded-full py-3 px-6 border-gray-500">
+    <p class="font-bold">School</p>
+    <p>{{ school }}</p>
+    <p class="font-bold">When</p>
+    <p>{{ startDate }} - {{ endDate }}</p>
+    <p class="font-bold">Research field</p>
+    <p>{{ researchField }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EducationCard',
+  props: {
+    school: {
+      type: String,
+    },
+    startDate: {
+      type: String, // Dateとかのほうがよさそう
+    },
+    endDate: {
+      type: String,
+    },
+    researchField: {
+      type: String,
+    },
+  },
+};
+</script>
--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,29 +1,47 @@
 <template>
   <div id="education">
     <p class="text-2xl md:text-4xl my-7 md:my-10 font-serif">Education</p>
-    <div class="text-sm md:text-lg w-3/5 mx-auto">
-      <div class="my-10 border-4 rounded-full py-3 px-6 border-gray-500">
-        <p class="font-bold">School</p>
-        <p>東京工業大学情報理工学院情報工学系知能情報コース</p>
-        <p class="font-bold">When</p>
-        <p>2020.3 - 2022.4</p>
-        <p class="font-bold">Research field</p>
-        <p>Bayesian Optimization Algorithm for black-box discrete function</p>
-      </div>
-      <div class="my-10 border-4 rounded-full py-3 px-6 border-gray-500">
-        <p class="font-bold">School</p>
-        <p>明治大学理工学部機械情報工学科</p>
-        <p class="font-bold">When</p>
-        <p>2016.3 - 2020.4</p>
-        <p class="font-bold">Research field</p>
-        <p>control engineering for two-wheeled vehicle</p>
-      </div>
+    <div
+      class="text-sm md:text-lg w-3/5 mx-auto"
+      v-for="school in schools"
+      v-bind:key="school.name"
+    >
+      <EducationCard
+        :school="school.name"
+        :startDate="school.startDate"
+        :endDate="school.endDate"
+        :researchField="school.researchField"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import EducationCard from '../atoms/EducationCard';
+
 export default {
   name: 'Education',
+  components: {
+    EducationCard,
+  },
+  data: function () {
+    return {
+      schools: [
+        {
+          name: '東京工業大学情報理工学院情報工学系知能情報コース',
+          startDate: '2020.4',
+          endDate: '2022.3',
+          researchField:
+            'Bayesian Optimization Algorithm for black-box discrete function',
+        },
+        {
+          name: '明治大学理工学部機械情報工学科',
+          startDate: '2016.4',
+          endDate: '2020.3',
+          researchField: 'control engineering for two-wheeled vehicle',
+        },
+      ],
+    };
+  },
 };
 </script>
