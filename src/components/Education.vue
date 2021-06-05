6db7e3c15e9c49aca9358866f7cbfdcaa282660b
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-06-05 14:22:59 +0900

    use card

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="education">
     <p class="text-2xl md:text-4xl my-7 md:my-10 font-serif">Education</p>
-    <div class="text-sm md:text-lg w-1/2 mx-auto">
-      <div class="p-1 md:p-3">
-        <li class="text-left">
-          Department of Mechanical Engineering Infomatics, Meiji Univ
-        </li>
-        <p>Bachelor's research: control engineering</p>
+    <div class="text-sm md:text-lg w-3/5 mx-auto">
+      <div class="my-10 border-4 rounded-full py-3 px-6 border-gray-500">
+        <p class="font-bold">School</p>
+        <p>東京工業大学情報理工学院情報工学系知能情報コース</p>
+        <p class="font-bold">When</p>
+        <p>2020.3 - 2022.4</p>
+        <p class="font-bold">Research field</p>
+        <p>Bayesian Optimization Algorithm for black-box discrete function</p>
       </div>
-      <div class="p-1 md:p-3">
-        <li class="text-left">
-          Computer Science, School of Computing, Tokyo Institute of Technology
-        </li>
-        <p>Master's research: Mathematical optimization, Genetic Algorithm</p>
+      <div class="my-10 border-4 rounded-full py-3 px-6 border-gray-500">
+        <p class="font-bold">School</p>
+        <p>明治大学理工学部機械情報工学科</p>
+        <p class="font-bold">When</p>
+        <p>2016.3 - 2020.4</p>
+        <p class="font-bold">Research field</p>
+        <p>control engineering for two-wheeled vehicle</p>
       </div>
     </div>
   </div>
