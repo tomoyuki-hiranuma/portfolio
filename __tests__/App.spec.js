aa5ccf6b942a782a3e30073f35b38c673e7ad20f
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-06-11 00:20:35 +0900

    create jest infra

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -1,9 +1,6 @@
-import { mount } from "@vue/test-utils";
-import App from '../src/App.vue';
+require('jsdom-global')();
 
 describe('Testing App Component', () => {
   it('is a Vue instance', () => {
-    const wrapper = mount(App)
-    expect(wrapper.isVueInstance).toBeTruthy()
-  })
+  });
 })
