58dad845b2a1c05afef7bd5deabc8f472413519c
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-06-12 01:04:04 +0900

    ignore vue recommend lint warning

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './assets/index.css';
-import VueGTag from "vue-gtag";
+import VueGTag from 'vue-gtag';
 
 Vue.config.productionTip = false;
 let VueScrollTo = require('vue-scrollto');
@@ -21,7 +21,7 @@ Vue.use(VueScrollTo, {
 });
 
 Vue.use(VueGTag, {
-  config: { id: "UA-199289393-3" }
+  config: { id: 'UA-199289393-3' },
 });
 
 new Vue({
