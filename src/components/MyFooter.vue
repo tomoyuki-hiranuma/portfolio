d667fc1ff5e4aff68a906bd0b2b4a2b9b2712b7b
tomoyuki-hiranuma <numanumatom@gmail.com>
2023-02-01 18:38:10 +0900

    fix: blank to a tag

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -4,10 +4,35 @@
       &copy;{{ thisYear }}- Tomoyuki Hiranuma. All rights reserved.
     </p>
     <div class="absolute right-10 top-1/3 text-sm md:text-base">
-      <a href="https://github.com/tomoyuki-hiranuma" class="m-2">Github</a>
-      <a href="https://twitter.com/__N_u_m_a" class="m-2">Twitter</a>
-      <a href="https://numa-blog.vercel.app/" class="m-2">Blog</a>
-      <a href="https://www.linkedin.com/in/tomoyuki-hiranuma-695266206/">
+      <a
+        href="https://github.com/tomoyuki-hiranuma"
+        class="m-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      <a
+        href="https://twitter.com/__N_u_m_a"
+        class="m-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+      <a
+        href="https://numa-blog.vercel.app/"
+        class="m-2"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Blog
+      </a>
+      <a
+        href="https://www.linkedin.com/in/tomoyuki-hiranuma-695266206/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         LinkedIn
       </a>
     </div>
